3be96d7f5caffe4c784c006490841d0a05555558
singh-swayam <swayam.singh@zeuslearning.com>
2026-06-15 18:50:22 +0530

    Refactor checkout.spec after feedback incorporation

--- DAY 3/tests/checkout.spec.ts
+++ DAY 3/tests/checkout.spec.ts
@@ -4,6 +4,7 @@ import { CartPage } from '../pages/CartPage';
 import { CheckoutPage } from '../pages/CheckoutPage';
 import { products } from '../test_data/products';
 import { loginAsStandardUser } from '../utils/testHelpers';
+import { STANDARD_USER } from '../test_data/user';
 
 test.describe('Checkout Tests', () => {
 
@@ -19,8 +20,7 @@ test.describe('Checkout Tests', () => {
   test('TC_010 - Checkout with valid details should reach overview page @checkout @regression', async ({ page }) => {
     await proceedToCheckoutForm(page);
     const checkoutPage = new CheckoutPage(page);
-    await checkoutPage.fillCheckoutDetails('Swayam', 'Singh', '400078');
-    await checkoutPage.continueCheckout();
+    await checkoutPage.submitCheckoutForm(STANDARD_USER.firstName!, STANDARD_USER.lastName!, STANDARD_USER.postalCode!);
     await checkoutPage.finishOrder();
     await checkoutPage.verifyOrderConfirmation();
   });
@@ -28,17 +28,15 @@ test.describe('Checkout Tests', () => {
   test('TC_011 - Checkout with missing first name should show validation error @checkout @negative', async ({ page }) => {
     await proceedToCheckoutForm(page);
     const checkoutPage = new CheckoutPage(page);
-    await checkoutPage.fillCheckoutDetails('', 'Singh', '400078');
-    await checkoutPage.continueCheckout();
-    await checkoutPage.verifyValidationMessage('First Name is required');
+    await checkoutPage.submitCheckoutForm('', STANDARD_USER.lastName!, STANDARD_USER.postalCode!);
+    await checkoutPage.verifyValidationMessage('Error: First Name is required');
   });
 
   test('TC_012 - Checkout with missing postal code should show validation error @checkout @negative', async ({ page }) => {
     await proceedToCheckoutForm(page);
     const checkoutPage = new CheckoutPage(page);
-    await checkoutPage.fillCheckoutDetails('Swayam', 'Singh', '');
-    await checkoutPage.continueCheckout();
-    await checkoutPage.verifyValidationMessage('Postal Code is required');
+    await checkoutPage.submitCheckoutForm(STANDARD_USER.firstName!, STANDARD_USER.lastName!, '');
+    await checkoutPage.verifyValidationMessage('Error: Postal Code is required');
   });
 
 });
